d0a2f6b7b03adde27bd903ce797262a0339f21ba
Phillip Klien <pklien@gmail.com>
2021-09-30 19:24:49 -0300

    Update wa.me-bookmarklet.js
    Changed wa.me address to api.whatsapp.com

--- wa.me-bookmarklet.js
+++ wa.me-bookmarklet.js
@@ -1,3 +1,3 @@
 // add this code as your bookmark
 
-javascript:(function(){ var waNumber = prompt('WhatsApp Telephone', '');if (waNumber != null) {waUrl = ('https://wa.me/' + decodeURIComponent(waNumber)).trim();waUrl = waUrl.replace(/\s/g, '');waUrl = waUrl.replace(/-/g, '');window.location.href = waUrl;} })();
+javascript:(function(){ var waNumber = prompt('WhatsApp Telephone', '');if (waNumber != null) {waUrl = ('https://api.whatsapp.com/send/?phone=' + decodeURIComponent(waNumber)).trim();waUrl = waUrl.replace(/\s/g, '');waUrl = waUrl.replace(/-/g, '');window.location.href = waUrl;} })();
